f54d0a1c311362c2cc5e9d74e12e80f3e6ea06b3
Manikanta <manikantaanyum966@gmail.com>
2024-12-14 20:20:03 +0530

    added_config

--- client/src/components/BibleVerse.tsx
+++ client/src/components/BibleVerse.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { BACKEND_URL } from "../config";
 
 interface VerseProps {
   id: string;
@@ -16,17 +17,18 @@ interface StoryProps {
 export const BibleVerse = () => {
   const [verse, setVerse] = useState<VerseProps | null>(null);
   const [biblestory, setBibleStory] = useState<StoryProps | null>(null);
+  const [toggle, setToggle] = useState(false);
 
   useEffect(() => {
     const fetchVerse = async () => {
-      const response = await fetch("http://localhost:8787/api/bible/verse");
+      const response = await fetch(`${BACKEND_URL}/api/bible/verse`);
       const result = await response.json();
       console.log(result);
       setVerse(result.lastVerse);
     };
 
     const fetchStory = async () => {
-      const response = await fetch("http://localhost:8787/api/bible/story");
+      const response = await fetch(`${BACKEND_URL}/api/bible/story`);
       const result = await response.json();
       console.log(result);
       setBibleStory(result.lastStory);
@@ -34,26 +36,15 @@ export const BibleVerse = () => {
 
     fetchVerse();
     fetchStory();
+    setToggle((p) => !p);
   }, []);
-
-  if (!verse === null && !biblestory === null) {
-    return (
-      <div className="flex justify-center h-[100vh]  items-center">
-        <div className="bg-purple-700 w-[600px]  p-8">
-          <p className="bg-white opacity-60 w-full h-14 font-bold text-3xl"></p>
-          <p className=" ml-auto bg-white opacity-40 mt-10 h-4 w-10 text-white font-bold italic"></p>
-        </div>
-      </div>
-    );
-  }
 
   return (
     <div>
-      <div>
+      <div className="bg-slate-100">
         <h1 className="text-2xl md:text-5xl text-center p-8 font-bold">
           Give 2 Minutes of your time to God
         </h1>
-        <p></p>
       </div>
       <div className="flex justify-center mt-6 md:mt-12">
         <div className="bg-purple-700 w-[400px] md:w-[700px] min-h-fit p-8 md:p-6">
@@ -65,7 +56,7 @@ export const BibleVerse = () => {
           </p>
         </div>
       </div>
-      <div className="mt-10 w-full p-8 md:p-20">
+      <div className="mt-10 w-full h-full bg-slate-100 p-8 md:p-20">
         <p className="text-2xl md:text-3xl font-bold">
           {biblestory?.storyName}{" "}
         </p>
